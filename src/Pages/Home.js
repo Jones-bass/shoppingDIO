f623b68091c8e26ab007e10d3d02154ad8887652
Jones-bass <jonesbass.tb@gmail.com>
2022-03-27 20:22:31 -0300

    Refatorando o Codigo

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Paper, Grid, Typography, List, makeStyles } from '@material-ui/core/';
 import Item from '../Components/Item';
 import Card from '../Components/Card';
@@ -7,58 +7,80 @@ import Card from '../Components/Card';
 
 const useStyles = makeStyles((theme) => ({
     root: {
-      flexGrow: 1,
-      marginTop: '5px',
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center'
-    },
-  }));
-
-const HomePage = ({ products }) => {
-    console.log(products);
-
-    const classes = useStyles();
-
-    return(
-        <Grid container spacing={3} className={classes.root}>
-            <Grid item xs={3}>
-                <Paper className={classes.paper}>
-                    <Typography variant='h5'>
-                    Categorias
-                    </Typography>
-                    <List>
-                        <Item 
-                            name="Times Nacionais"
-                            details="3"
-                        />
-                        <Item 
-                            name="Times Internacionais"
-                            details="4"
-                        />
-                        <Item 
-                            name="Times Históricos"
-                            details="5"
-                        />
-                    </List>
-                </Paper>
-            </Grid>
-            <Grid container xs={9} spacing={3} className={classes.root}>
-                {products.map(item => {
-                    return(
-                        <Card key={item.id_product} name={item.name_product} price={item.price} image={item.image}>
-                            {item.name_product}
-                        </Card>
-                    )
-                })}
-            </Grid>
-        </Grid>
-    )
-}
-
-const mapStateToProps = state => ({
-    products: state.products 
-})
-
-export default connect(mapStateToProps)(HomePage);
+        flexGrow: 1,
+        marginTop: '5px',
+      },
+      paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center'
+      },
+    }));
+  
+  const HomePage = () => {
+      const products = useSelector(state => state.products)
+      const classes = useStyles();
+  
+      const categorys = products.map(
+          category => {
+              const container = { };
+              container['id'] = category.id_categorys;
+              container['name'] = category.name_categorys;
+              return container;
+          }
+      )
+  
+      const category = categorys.map(JSON.stringify)
+                      .filter(function(item, index, arr){
+                          return arr.indexOf(item, index + 1) === -1;
+                      })
+                      .map(JSON.parse)
+  
+      const arrayCategory = categorys.map(category => category.name)
+      let count = { };
+  
+      for(let i = 0; i < arrayCategory.length; i++){
+          {
+              let key = arrayCategory[i];
+              count[key] = (count[key] ? count[key] + 1 : 1)
+          }
+      }
+  
+      return(
+          <Grid container spacing={3} className={classes.root}>
+              <Grid item xs={3}>
+                  <Paper className={classes.paper}>
+                      <Typography variant='h5'>
+                          Categorias
+                      </Typography>
+                      <List>
+                          {category.map(
+                              category => {
+                                  return (
+                                      <Item
+                                          key = {category.id} 
+                                          name= {category.name}
+                                          details={count[category.name]}
+                                      />
+                                  )
+                              }
+                          )}
+                      </List>
+                  </Paper>
+              </Grid>
+              <Grid container xs={9} spacing={3} className={classes.root}>
+                  {products.map(item => {
+                      return(
+                          <Card
+                              key={item.id_product}
+                              product={item}
+                          >
+                              {item.name_product}
+                          </Card>
+                      )
+                  })}
+              </Grid>
+          </Grid>
+      )
+  }
+  
+  export default HomePage;
